13fb672edd62d6e44bfe254ad019ee24f740772f
Jacques Nel <jmnel92@gmail.com>
2020-06-21 20:24:22 -0400

    IEX import is working and done.

--- realtime/src/strategies/iex_import.py
+++ realtime/src/strategies/iex_import.py
@@ -7,15 +7,21 @@ import requests
 import io
 import zipfile
 import re
+import logging
 
 import quandl
 
 import settings
+from ticker_filter import TickerFilter
 
 quandl.ApiConfig.api_key = settings.QUANDL_API_KEY
 
+logger = logging.getLogger(__name__)
+
 
 def prepare_database() -> sqlite3.Connection:
+
+    logger.info(f'Preparing database {settings.DATABASE_NAME}...')
 
     db = sqlite3.connect(settings.DATA_DIRECTORY / settings.DATABASE_NAME)
 
@@ -23,7 +29,6 @@ def prepare_database() -> sqlite3.Connection:
     db.execute('DROP TABLE IF EXISTS iex_trade_reports;')
     db.execute('DROP TABLE IF EXISTS iex_days;')
     db.execute('DROP TABLE IF EXISTS iex_symbols;')
-#    db.execute('DROP TABLE IF EXISTS iex_trade_reports_day_symbols;')
 
     # Create days meta table.
     db.execute('''
@@ -57,22 +62,22 @@ CREATE TABLE iex_trade_reports(
 );
 ''')
 
+    logger.info('Done')
+
     return db
 
 
-def parse_csv(db):
+def parse_csv(db: sqlite3.Connection, symbols_meta):
+    """
+    Parse all csv files for all available days.
+
+    """
+
+    logger.info('Parsing csv files for days.')
+
     csv_directory = settings.DATA_DIRECTORY / 'csv_test'
 
-    # Get list of days be enumerating CSV files in directory.
-#    days = list()
-#    for f in os.listdir(csv_directory):
-#        csv_path = csv_directory / f
-#        if csv_path.is_file and csv_path.suffix == '.csv':
-#            days.append(csv_path.name)
-
-#    pprint(days)
-#    exit()
-
+    # Get list of days by enumerating csv files in directory.
     for f in os.listdir(csv_directory):
         csv_path = csv_directory / f
         if csv_path.is_file and csv_path.suffix == '.csv':
@@ -80,32 +85,24 @@ def parse_csv(db):
             day = csv_path.name[:-4]
             day = '-'.join((day[:4], day[4:6], day[6:8]))
 
-            print(day)
-
             db.execute('INSERT INTO iex_days(date) VALUES(?);', (day,))
             db.commit()
             day_id = db.execute('SELECT last_insert_rowid();').fetchone()[0]
-            print(day_id)
 
-            print('\tEnumerating symbols...')
-            with open(csv_path, 'r') as csv_file:
-                rows = csv.reader(csv_file, delimiter=',')
-                _, rows, _, _ = zip(*list(rows))
-
-            rows = set(rows)
-            rows = tuple((r,) for r in rows)
-            db.executemany('''
-INSERT OR IGNORE INTO iex_symbols(symbol) VALUES(?)
-;''', rows)
-            db.commit()
-            print('\tDone.')
+            logger.info(f'Found day {day} @ {f}.')
 
             with open(csv_path, 'r') as csv_file:
-                rows = csv.reader(csv_file, delimiter=',')
+                reader = csv.reader(csv_file, delimiter=',')
                 date_str = '-'.join((f[:4], f[4:6], f[6:8]))
-                rows = list(rows)
 
-                rows = list((date_str, r[0], r[1], r[2], r[3]) for r in rows)
+                rows = list()
+                for idx, row in enumerate(reader):
+                    timestamp, symbol, price, size = row
+                    qdl_symbol = symbol.replace('.', '_').replace('-', '_')
+                    if qdl_symbol in symbols_meta:
+                        rows.append((date_str, timestamp, qdl_symbol, price, size))
+
+                logger.info(f'Storing {len(rows)} of {idx+1} messages to database.')
 
                 db.executemany('''
 INSERT INTO iex_trade_reports(day, timestamp, symbol, price, size)
@@ -114,27 +111,32 @@ VALUES(?, ?, ?, ?, ?);
                 db.commit()
 
 
-def get_quandl_ticker_info(db):
+def get_quandl_meta(db):
+
+    logger.info('Downloading Quandl EOD metadata.')
 
     META_ENDPOINT = 'https://www.quandl.com/api/v3/databases/EOD/metadata?api_key={}'
     response = requests.get(META_ENDPOINT.format(settings.QUANDL_API_KEY))
     if not response.ok:
-        print('error')
-
-#    pprint(response.content)
+        logger.critical(f'Quandl metadata endpoint request error {response.status_code}.')
+        exit()
 
     exchange_re = re.compile(r'<p><b>Exchange<\/b>: ([\w ]+)<\/p>')
 
     ss = io.BytesIO(response.content)
     zf = zipfile.ZipFile(ss)
+
+    logger.info('Processing metadata')
+    count = 0
     with zf.open('EOD_metadata.csv', 'r') as f:
         f2 = io.TextIOWrapper(f, encoding='utf-8')
         reader = csv.reader(f2, delimiter=',')
-        h = next(reader)
-        pprint(h)
+        next(reader)
 
         rows = list()
         for idx, row in enumerate(list(reader)):
+
+            count += 1
 
             symbol = row[0]
             name = row[1][:-38 - len(row[0])]
@@ -144,45 +146,32 @@ def get_quandl_ticker_info(db):
             m = exchange_re.search(row[2])
             if m:
                 exchange = m.groups()[0]
+
+            # Symbol AA_P has no description, exchange, or data.
             else:
-                print(f'no match: "{row[2]}"')
-                print(f'sym: {row[0]}')
                 continue
 
-        #    response.ok()
+            if TickerFilter.filter(symbol, name, exchange):
+                rows.append((symbol, qdl_code, name, exchange))
 
-        #    reader = csv.reader(io.StringIO(tickers_reponse.text), delimiter=',')
+    symbols_meta = {r[0]: r for r in rows}
 
-        # This the url of the API endpoint for the master list of tickers.
-        #    TICKER_URL = 'https://s3.amazonaws.com/quandl-production-static/end_of_day_us_stocks/ticker_list.csv'
+    db.executemany('''
+INSERT INTO iex_symbols(symbol, qdl_code, name, exchange)
+VALUES(?, ?, ?, ?);
+''', rows)
+    db.commit()
 
-        # Get the csv file using HTTP request.
-        #    tickers_reponse = requests.get(TICKER_URL)
+    logger.info(f'Found {count} symbols, filtered to {len(rows)}')
+    logger.info('Done')
 
-        #    reader = csv.reader(io.StringIO(tickers_reponse.text), delimiter=',')
-        #    h = next(reader)
-        #    pprint(h)
-        #    ticker_list = list(reader)
-
-        #    ticker_dict = {r[0]: (r[1], r[2], r[3], r[4]) for r in ticker_list}
-
-        #    pprint(ticker_dict)
-
-        #    symbols = tuple(zip(*db.execute('SELECT symbol from iex_symbols;').fetchall()))[0]
-
-        #    for s in symbols:
-        #        if s not in ticker_dict:
-        #            print(f'missing: {s}')
-
-        # Filter out non-common stocks.
-        #    ticker_list = list(
-        #        filter(lambda row: TickerFilter.filter(row[0], row[2], row[3]), ticker_list))
+    return symbols_meta
 
 
 def main():
     db = prepare_database()
-#    parse_csv(db)
-    get_quandl_ticker_info(db)
+    symbols_meta = get_quandl_meta(db)
+    parse_csv(db, symbols_meta)
     db.close()
 
 
--- realtime/src/strategies/settings.py
+++ realtime/src/strategies/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import logging
 
 ROOT_DIRECTORY = Path(__file__).absolute().parents[2]
 DATA_DIRECTORY = ROOT_DIRECTORY / 'data'
@@ -9,3 +10,18 @@ QUANDL_API_KEY = os.getenv('QUANDL_API_KEY')
 ALPHA_VANTAGE_API_KEY = '63D5Z3C3GBRHSENV'
 
 EXCHANGES = {'NASDAQ', 'NYSE', 'NYSE', 'NYSE MKT', 'NYSE Arca'}
+
+
+def setup_logger():
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+
+setup_logger()
